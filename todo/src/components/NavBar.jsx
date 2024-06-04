b768de469b78baf5e09695fbb9a15da9e6a98b00
KazbundaYTB <100788815+KazbundaYTB@users.noreply.github.com>
2024-06-04 09:37:17 +0200

    Changed button texts.

--- todo/src/components/NavBar.jsx
+++ todo/src/components/NavBar.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import { IoLogInOutline } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa";
 
 export default function NavBar({ setShowPopup, handleSignOut }) {
   return (
     <div className="flex justify-around items-center w-full">
       <button 
         onClick={() => setShowPopup(true)} // Zde jsme nastavili, aby se popup vždy otevřel při kliknutí na tlačítko
-        className="p-2 bg-blue-500 rounded"
+        className="p-2 text-2xl text-white bg-blue-500 rounded"
       >
-        Add Quest
+        <FaPlus />
       </button>
       <button 
         onClick={handleSignOut} 
         className="p-2 bg-red-500 rounded"
       >
-        Sign Out
+        <IoLogInOutline />
       </button>
     </div>
   );
