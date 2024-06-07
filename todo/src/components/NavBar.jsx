d192c3a4cd42f422642d24036094d4aca3d12a6d
Kazbunda <gejmraopi11@gmail.com>
2024-06-07 21:11:41 +0200

    Changed button designs.

--- todo/src/components/NavBar.jsx
+++ todo/src/components/NavBar.jsx
@@ -7,22 +7,22 @@ export default function NavBar({ setShowPopup, handleSignOut, questArrayLength }
   return (
     <div className="flex justify-around items-center w-full">
       <div className="relative">
-        <span className="absolute top-0 right-0 transform translate-x-1/5 -translate-y-1/3 text-sm text-white px-2">
+        <span className="absolute top-0 right-0 transform translate-x-1/4 -translate-y-1/5 text-sm text-black font-bold px-2">
           {questArrayLength}
         </span>
-        <button className="p-2 rounded">
-          <FaRegFileAlt style={{ color: '#10B981' }} /> {/* emerald-500 color */}
+        <button className="p-2 bg-white text-emerald-500 rounded-md ">
+          <FaRegFileAlt /> {/* emerald-500 color */}
         </button>
       </div>
       <button 
         onClick={() => setShowPopup(true)} 
-        className="p-2 text-2xl text-white bg-blue-500 rounded-full"
+        className="p-2 text-2xl text-white bg-emerald-500 rounded-full"
       >
         <IoMdAdd style={{ fontSize: '135%' }}/>
       </button>
       <button 
         onClick={handleSignOut} 
-        className="p-2 bg-red-500 rounded"
+        className="p-2 bg-white text-emerald-500 rounded-md"
       >
         <IoLogOutOutline />
       </button>
